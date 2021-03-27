be104b35bb69a8dc3e1f76d0dbaa68a17f115ea1
Sohel Islam Imran <sohelislamimran@gmail.com>
2021-03-27 17:29:07 +0600

    With Auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,7 +10,6 @@ const Login = () => {
 
     const history = useHistory();
     const location = useLocation();
-
     const { from } = location.state || { from: { pathname: "/" } };
 
     const [newUser, setNewUser] = useState(false);
